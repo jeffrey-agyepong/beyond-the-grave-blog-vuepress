cadc1935b8d84e40d6e9f5786a2313998ef66b29
Jeffrey777A <44594550+Jeffrey777A@users.noreply.github.com>
2020-12-23 05:07:22 +0000

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'BEYOND THE GRAVE',
-  base: 'https://beyondthegrave.netlify.app/',
+  base: 'beyondthegrave.netlify.app',
   description: 'Blog example for Mediumish theme with VuePress',
   logo: './assets/img/logo.png',
   theme: require.resolve('../../'),
