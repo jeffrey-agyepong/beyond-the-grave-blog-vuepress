aeaa8cc7e225855eb55e0af7ef10a09909d51af2
Jeffrey777A <44594550+Jeffrey777A@users.noreply.github.com>
2021-02-27 01:54:47 +0000

    .

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,9 +7,31 @@ module.exports = {
     ['link', { rel: 'icon', sizes: '32x32', href: './assets/img/favicon.ico' }],
   ],
   plugins:{ 
-    '@vuepress/plugin-google-analytics': {
-      ga: 'G-B652H7R518',
-    },
+    head: [
+      ['script', {
+          src: 'https://config.metomic.io/config.js?id=prj:79c6192b-4005-4261-89c2-430b39551375', 
+          crossorigin: 'anonymous',
+          charset: 'utf-8'
+      }],
+      ['script', {
+          src: 'https://consent-manager.metomic.io/embed.js', 
+          crossorigin: 'anonymous',
+          charset: 'utf-8'
+      }],
+      ['script', {
+          async: true,
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-455B6ESQJK'
+      }],
+      ['script', {}, `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+      
+          gtag('config', 'G-455B6ESQJK');
+      `],
+ 
+  ],
+
     'seo':{
       siteTitle: (_, $site) => $site.title,
       title: $page => $page.title,
